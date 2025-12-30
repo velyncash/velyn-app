65d1120be3166334081a58ebcedde4c824d20308
Velyn Cash <dev@velyn.cash>
2025-12-30 23:41:33 +0700

    Create tokens.ts

---
+++ src/theme/tokens.ts
@@ -0,0 +1,12 @@
+export const tokens = {
+  radius: 18,
+  radiusLg: 24,
+  padding: 16,
+  shadow: {
+    shadowColor: "#000",
+    shadowOpacity: 0.08,
+    shadowRadius: 16,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 6,
+  },
+};
