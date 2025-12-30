fb79e4c0952759225f5a8b33674100be24676823
Velyn Cash <dev@velyn.cash>
2025-12-30 23:31:25 +0700

    Create index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Velyn App</Text>
+      <Text>Mobile scaffold ready</Text>
+    </View>
+  );
+}
